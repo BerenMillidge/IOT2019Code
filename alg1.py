aad3aef18f31f1ac9128a7753f2d45ff2c97f776
Beren Millidge <bmillidgework@gmail.com>
2019-02-23 16:18:11 +0000

    Add files via upload

---
+++ alg1.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while(1):
+
+    # Take each frame
+    _, frame = cap.read()
+
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # define range of blue color in HSV
+    lower_red1 = np.array([0,70,50])
+    lower_red2 = np.array([10,255,255])
+    upper_red1 = np.array([170,70,50])
+    upper_red2 = np.array([180,255,255])
+
+    # Threshold the HSV image to get only blue colors
+    mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+    mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+    mask = mask1 | mask2
+
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
